f14dfe5e6de062e2d7b039afaf1b00180a63ec6e
MahrAbrarThind <mahrabrarthind@gmail.com>
2024-11-13 16:01:14 +0500

    changed vercel.json a little

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/signup', async (req, res) => {
  const { username, email, password } = req.body;
- const hashedPassword = await bcrypt.hash(password, 10);
+ const hashedPassword = await bcryptjs.hash(password, 10);
  const newUser = new User({ username, email, password: hashedPassword 
 });
  await newUser.save();
@@ -21,7 +21,7 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    if (!user || !(await bcryptjs.compare(password, user.password))) {
     return res.status(401).json({ message: 'Invalid credentials' });
     }
     res.status(200).json({ message: 'Login successful', user });
